25e1036d9ba3ca185f85296e86b93f29a45c124e
a.pras@utwente.nl <a.pras@utwente.nl>
2022-08-02 18:05:26 +0200

    Clean up

---
+++ examples/LED/DCC/DCC.ino
@@ -0,0 +1,29 @@
+//******************************************************************************************************
+//
+// Test sketch for the AP_DCC_LED library
+// 2019/02/24 / 2021/06/15 / 2022/07/20 AP
+//
+//******************************************************************************************************
+#include <Arduino.h>
+#include <AP_DCC_LED.h>
+
+DCC_Led myLed;                           // Instantiate the LED object
+
+const uint8_t ledPin = LED_BUILTIN;      // Can be used for most boards (13)
+
+
+void setup() {
+  delay(2000);
+  myLed.attach(ledPin);
+ 
+  myLed.start_up();
+//  myLed.activity();
+//  myLed.feedback();
+//  myLed.flashSlow();
+//  myLed.flashFast();
+}
+
+
+void loop() {
+myLed.update();
+}
---
+++ examples/LED/Flash/Flash.ino
@@ -0,0 +1,31 @@
+//******************************************************************************************************
+//
+// Test sketch for the AP_DCC_LED library
+// The onboard LED should blink 5 times, followed by 2 seconds off
+// 2019/02/24 / 2021/06/15 / 2022/07/20 AP
+//
+//******************************************************************************************************
+#include <Arduino.h>
+#include <AP_DCC_LED.h>
+
+Flash_Led myLed;                      // Instantiate the LED object
+
+const uint8_t ledPin = LED_BUILTIN;   // Can be used for most boards (13)
+
+
+void setup() {
+  myLed.attach(ledPin);
+
+  myLed.flashOntime = 2;              // 200ms (all times are in 100ms steps)
+  myLed.flashOfftime = 5;             // 500ms
+  myLed.flashCount = 5;
+  myLed.flashPause = 20;              // 2 seconds
+  myLed.mode = neverStopFlashing;  
+   
+  myLed.flash();
+}
+
+
+void loop() {
+myLed.update();
+}
